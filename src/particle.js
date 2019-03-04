0a58a6bd05c9eb29a20193e397ef8df7b02db2e1
Sindre Klavestad <sindreklavestad@Sindres-MacBook-Pro.local>
2019-03-04 23:07:08 +0100

    update

--- src/particle.js
+++ src/particle.js
@@ -2,11 +2,18 @@ var ParticleAPI = require('particle-api-js');
 
 export default class Particle {
     constructor({
-        username,
-        password
-    }) {
+            username,
+            password
+        },
+        device,
+        name,
+        argument,
+    ) {
         this.username = username,
-            this.password = password
+            this.password = password,
+            this.device = device,
+            this.name = name,
+            this.argument = argument
     }
 
     async login() {
@@ -28,11 +35,9 @@ export default class Particle {
     async devices() {
         const particle = new ParticleAPI();
         const token = await this.login();
-        const devices = particle.listDevices({
+        const devices = await particle.listDevices({
             auth: token
-        });
-
-        devices.then(
+        }).then(
             function (devices) {
                 return devices
             },
@@ -41,6 +46,55 @@ export default class Particle {
             }
         );
         return await devices;
+    }
+
+    async deviceId() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const deviceName = this.device;
+        const deviceId = await particle.listDevices({
+            auth: token
+        }).then(
+            function (devices) {
+                let id;
+                devices.body.forEach(o => {
+                    if (o.name === deviceName) {
+                        id = {
+                            id: o.id
+                        };
+                    } else {
+                        return {
+                            message: "did not find nothing"
+                        };
+                    }
+                });
+
+                return id;
+            },
+            function (err) {
+                console.log('Listing all the devices failed for some reason: ', err);
+            }
+        );
+        return await deviceId;
+    }
+
+    async getAttributes() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const getAttributes = particle.getDevice({
+            deviceId: this.device,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Device attrs retrieved successfully:', data);
+                return data;
+            },
+            function (err) {
+                console.log('API call failed: ', err);
+                return err;
+            });
+
+        return await getAttributes;
     }
 
     async eventstream() {
@@ -61,5 +115,172 @@ export default class Particle {
         );
 
         return await eventstream;
+    }
+
+    async call() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const call = particle.callFunction({
+            deviceId: this.device,
+            name: this.name,
+            argument: this.argument,
+            auth: token
+        });
+
+        call.then(
+            function (data) {
+                console.log('Function called succesfully:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await call;
+    }
+
+    async read() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const read = particle.getVariable({
+            deviceId: this.device,
+            name: this.name,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Successfully recieved variable data:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await read;
+    }
+
+    async claim() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const claim = particle.claimDevice({
+            deviceId: this.device,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('device claim data:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await claim;
+    }
+
+    async remove() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const remove = particle.removeDevice({
+            deviceId: this.device,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Removed device:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await remove;
+    }
+
+    async rename() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const rename = particle.removeDevice({
+            deviceId: this.device,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Renamed device OK:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await rename;
+    }
+
+    async signal() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const signal = particle.signalDevice({
+            deviceId: this.device,
+            signal: true,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Signaling device OK:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await signal;
+    }
+
+    async signalOff() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const signal = particle.signalDevice({
+            deviceId: this.device,
+            signal: false,
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Signaling device OK:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred:', err);
+                return err;
+            });
+
+        return await signal;
+    }
+
+    async signal10() {
+        //TODO
+
+    }
+
+    async flash() {
+        const particle = new ParticleAPI();
+        const token = await this.login();
+        const flash = particle.flash({
+            deviceId: this.device,
+            files: {
+                /* TODO */
+            },
+            auth: token
+        }).then(
+            function (data) {
+                console.log('Device flashing started successfully:', data);
+                return data;
+            },
+            function (err) {
+                console.log('An error occurred while flashing the device:', err);
+                return err;
+            });
+
+        return await flash;
     }
 }
--- src/router.js
+++ src/router.js
@@ -22,12 +22,74 @@ router.get('/api/devices', async (req, res) => {
     })
 })
 
+router.get('/api/deviceId/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const devices = await particle.deviceId()
+
+    res.send({
+        devices
+    })
+})
+
+router.get('/api/attributes/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const attributes = await particle.getAttributes()
+    res.send({
+        attributes: attributes
+    })
+})
+
+router.get('/api/device/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const attributes = await particle.getAttributes()
+    res.send({
+        attributes: attributes
+    })
+})
+
+router.get('/api/signal/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const signal = await particle.signal()
+    res.send({
+        signal: signal
+    })
+})
+
+router.get('/api/signalOff/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const signal = await particle.signalOff()
+    res.send({
+        signal: signal
+    })
+})
+
+router.get('/api/signal10/:device', async (req, res) => {
+    const deviceName = req.params.device
+    const particle = new Particle(CREDENTIALS, deviceName);
+
+    const signal = await particle.signal10()
+    res.send({
+        signal: signal
+    })
+})
+
 router.get('/api/eventstream', async (req, res) => {
     const event = await particle.eventstream()
 
     let array = [];
 
     event.on('event', function (data) {
+        console.log(data);
         while (array.length < 10) {
             array.push(data);
         }
@@ -36,6 +98,30 @@ router.get('/api/eventstream', async (req, res) => {
         })
     });
 
+})
+
+router.get('/api/read/:device/:name', async (req, res) => {
+    const deviceName = req.params.device
+    const name = req.params.name
+    const particle = new Particle(CREDENTIALS, deviceName, name);
+
+    const variable = await particle.read()
+    res.send({
+        variable: variable
+    })
+})
+
+router.get('/api/call/:device/:name/:argument', async (req, res) => {
+    const deviceName = req.params.device
+    const name = req.params.name
+    const argument = req.params.argument
+
+    const particle = new Particle(CREDENTIALS, deviceName, name, argument);
+
+    const call = await particle.call()
+    res.send({
+        responce: call
+    })
 })
 
 export default router
